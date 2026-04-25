6c4bbfa911cd86c9d7a7260bd326818045192ef2
Max Degterev <max@degterev.me>
2026-04-25 17:38:06 +0200

    fix: add onReady and onShutdown options to createGraceful

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,22 @@ import type { Server } from 'http';
 
 const SIGNALS = ['SIGTERM', 'SIGINT'] as const;
 
-const isDebug = process.env.DEBUG?.includes('express-graceful');
-
 type Options = {
   host?: string,
   port?: number,
   timeout?: number,
   logger?: (message: string) => void,
+  onReady?: (event: string) => void,
+  onShutdown?: (event: string) => void,
 };
-
-type Handler = (event: string) => void;
 
 type GracefulInstance = {
   middleware: (req: Request, res: Response, next: NextFunction) => void,
-  start: (app: Application, options?: Options, handler?: Handler) => void,
+  start: (app: Application) => void,
 };
 
-export const createGraceful = (): GracefulInstance => {
+export const createGraceful = (options?: Options): GracefulInstance => {
+  const { host, port = 3000, timeout = 1000, logger, onReady, onShutdown } = options ?? {};
   let isShuttingDown = false;
   let httpListener: Server;
 
@@ -29,35 +28,34 @@ export const createGraceful = (): GracefulInstance => {
     res.status(502).send('Server is shutting down.');
   };
 
-  const getShutdownHandler = (event: string, timeout: number, onClose?: Handler) => () => {
+  const getShutdownHandler = (event: string) => () => {
     if (isShuttingDown) return false;
 
-    if (isDebug) console.info(`Received ${event}, shutting down.`);
+    logger?.(`Received ${event}, shutting down...`);
 
     isShuttingDown = true;
     httpListener.close(() => {
-      if (isDebug) console.info('Closed remaining connections.');
+      logger?.('Closed remaining connections.');
       process.exit(0);
     });
 
-    if (typeof onClose === 'function') onClose(event);
+    onShutdown?.(event);
 
     setTimeout(() => {
-      if (isDebug) console.info("Couldn't close connections in time, forcefully shutting down.");
+      logger?.("Couldn't close connections in time, forcefully shutting down.");
       process.exit(1);
     }, timeout);
 
     return true;
   };
 
-  const start = (app: Application, options?: Options, handler?: Handler) => {
-    const { host, port = 3000, timeout = 1000, logger } = options ?? {};
-
+  const start = (app: Application) => {
     const message = `Server listening on http://${host ?? 'localhost'}:${port}`;
 
     const sendEvents = (text: string) => {
       logger?.(text);
       if (process.connected) process.send?.('ready');
+      onReady?.('ready');
     };
 
     if (host) {
@@ -66,7 +64,7 @@ export const createGraceful = (): GracefulInstance => {
       httpListener = app.listen(port, () => sendEvents(message));
     }
 
-    SIGNALS.forEach((event) => process.on(event, getShutdownHandler(event, timeout, handler)));
+    SIGNALS.forEach((event) => process.on(event, getShutdownHandler(event)));
   };
 
   return { middleware, start };
